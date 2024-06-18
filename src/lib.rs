065fbf4b5cf57a9e8e545d90ec9d689ccdf6e888
Arthur Lee <arthur.lee945@gmail.com>
2024-06-17 21:06:18 -0500

    starting on sub endpoing

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
 // impl Responder ==> If type can convert into a HttpResponse
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         // Route::new().guard(guard::Get()) == web::get()
-        App::new().route("/health_check", web::get().to(health_check))
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,6 +15,51 @@ async fn health_check_test() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=monkey%20struct&email=monkeystruct_test%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", &addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute post request");
+
+    //assert_eq!(response.status(), reqwest::StatusCode::ACCEPTED);
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_for_bad_form_data() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=monkey%man", "Missing the email"),
+        ("email=monk%40gmail.com", "Missing the name"),
+        ("", "Missing Both"),
+    ];
+
+    for (query, msg) in test_cases {
+        let resp = client
+            .post(format!("{}/subscriptions", &addr))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(query)
+            .send()
+            .await
+            .expect("Failed to execute post request");
+        assert_eq!(
+            400,
+            resp.status().as_u16(),
+            "Expected API to fail returning 400 Bad Request for {}",
+            msg
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
