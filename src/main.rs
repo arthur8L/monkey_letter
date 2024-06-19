4598f9eab363bacde96b72026c146489ce4f81a0
Arthur Lee <arthur.lee945@gmail.com>
2024-06-19 18:32:46 -0500

    add base logger impl

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use monkey_letter::{configuration, startup::run};
 use sqlx::PgPool;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = configuration::get_configuration().expect("Failed to read configuration.");
+
     let conn_pool = PgPool::connect(&config.database.connection_str())
         .await
         .expect("Failed to connect to Postgres");
