0b4f56b0ebef9093e065fb4bfae3bb5ed617e532
Arthur Lee <arthur.lee945@gmail.com>
2024-06-19 19:17:40 -0500

    update base logger impl

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,6 +10,14 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    let req_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding username: {}, email: {} as a new subscriber.",
+        req_id,
+        form.name,
+        form.email
+    );
+    log::info!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -23,9 +31,15 @@ pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) ->
     .execute(db_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request_id {} -New Subscriber is succeessfully added",
+                req_id
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(err) => {
-            println!("Failed to execute query: {}", err);
+            log::error!("request_id {} - Failed to execute query: {:?}", req_id, err);
             HttpResponse::InternalServerError().finish()
         }
     }
