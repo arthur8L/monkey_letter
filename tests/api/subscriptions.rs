ce034e912b70eb79cc64272fa5b13923e0098d41
Arthur Lee <arthur.lee945@gmail.com>
2024-06-23 19:36:49 -0500

    refactoring email link test

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -112,18 +112,8 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     app.post_subscriptions(body.into()).await;
     let email_req = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_req.body).unwrap();
 
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-        links[0].as_str().to_owned()
-    };
+    let link = app.get_confirmation_link(email_req);
 
-    let html_link = get_link(body["HtmlBody"].as_str().unwrap());
-    let text_link = get_link(body["TextBody"].as_str().unwrap());
-
-    assert_eq!(html_link, text_link);
+    assert_eq!(link.html, link.plain_text);
 }
--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,4 +1,3 @@
-use reqwest::Url;
 use wiremock::{
     matchers::{method, path},
     Mock, ResponseTemplate,
@@ -30,22 +29,8 @@ async fn link_returned_by_sub_returns_200_when_called() {
 
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+    let link = app.get_confirmation_link(email_request);
 
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
-
-    let raw_confirmation_link = &get_link(body["HtmlBody"].as_str().unwrap());
-    let mut confirmation_link = Url::parse(raw_confirmation_link).unwrap();
-    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
-
-    confirmation_link.set_port(Some(app.port)).unwrap();
-    let res = reqwest::get(confirmation_link).await.unwrap();
+    let res = reqwest::get(link.html).await.unwrap();
     assert_eq!(res.status().as_u16(), 200);
 }
