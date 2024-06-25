efc3a2d33387765068cbdc1cbbb8a6ea6b932a60
Arthur Lee <arthur.lee945@gmail.com>
2024-06-25 18:36:53 -0500

    adding newsletter publish functionality

--- src/email_client.rs
+++ src/email_client.rs
@@ -36,7 +36,7 @@ impl EmailClient {
     }
     pub async fn send_email(
         &self,
-        recipient: SubscriberEmail,
+        recipient: &SubscriberEmail,
         subject: &str,
         html_content: &str,
         text_content: &str,
@@ -125,7 +125,7 @@ mod tests {
             .await;
 
         let _ = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         //assert
@@ -144,7 +144,7 @@ mod tests {
             .await;
 
         let result = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
 
         assert_ok!(result);
@@ -163,7 +163,7 @@ mod tests {
             .await;
 
         let result = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
         assert_err!(result);
     }
@@ -181,7 +181,7 @@ mod tests {
             .await;
 
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
         assert_err!(outcome);
     }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -190,7 +190,7 @@ pub async fn send_confirmation_email(
     );
     email_client
         .send_email(
-            new_subscriber.email,
+            &new_subscriber.email,
             "Welcome!",
             &format!(
                 "Welcome to our news letter!<br/>\
